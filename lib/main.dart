ab6b09fac7138ea7f47c31f730eb82533075c292
Adam556678 <mahmudramadan75@yahoo.com>
2024-07-07 14:20:10 +0300

    firebase registeration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:e_commerce/app.dart';
+import 'package:e_commerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) => Get.put(AuthenticationRepository()));
   runApp(const App());
 }
